8aa62cbc5911f9996ce5fa5c0888949718277251
Justin <justpunz@live.com>
2016-12-30 02:11:32 -0800

    Added Skills content

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ $( document ).ready(function() {
 	        scrollTop: eval($('#' + $(this).attr('target')).offset().top)
 	    }, 1000);
 	});
+
+	// Trigger for skills
+
+	$('#skill-html').progress();
+	$('#skill-css').progress();
+	$('#skill-js').progress();
+	$('#skill-emberjs').progress();
+	$('#skill-java').progress();
+	$('#skill-linux').progress();
+	$('#skill-vba').progress();
+	$('#skill-photoshop').progress();
+
+
 });
