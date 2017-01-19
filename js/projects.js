bdf2ae0fd08b49398aedc98800f3d52326a4e15f
Justin <justpunz@live.com>
2017-01-18 18:52:15 -0800

    Changed Colors and added Nav to projects

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
+	$('.side-nav-bar.sidebar').sidebar({
+		onChange: function(){
+			$('#sticky').transition('fade');
+		},
+		scrollLock: true,
+		transition: 'overlay'
+	});
+
+	$('.ui.sticky').sticky({
+		offset: 32
+	});
 
 })
+
+function toggleSideNav(){
+	$('.ui.labeled.icon.sidebar').sidebar('toggle');
+}
